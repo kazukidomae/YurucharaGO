3f7c8441c1708d40048954b4a9c367a8fa622ad3
nakajima.shunsuke <grimmalice10@gmail.com>
2018-05-31 14:24:32 +0900

    [add]create_ region_table migrate

---
+++ database/migrations/2018_05_31_051510_create_region_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('region', function (Blueprint $table) {
+            $table->increments('RegionID');
+            $table->string('RegionName', 20);
+            $table->integer('PrefectureNumber');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('region');
+    }
+}
