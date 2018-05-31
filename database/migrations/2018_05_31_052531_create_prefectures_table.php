8ae989042f53cf7562639fc6565184b2edca93bb
nakajima.shunsuke <grimmalice10@gmail.com>
2018-05-31 19:55:44 +0900

    [add]create_prefectures_table migrate

---
+++ database/migrations/2018_05_31_052531_create_prefectures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrefecturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prefectures', function (Blueprint $table) {
+            $table->increments('PrefecturesID');
+            $table->integer('RegionID')->unsigned();
+            $table->string('PrefecturesName',20);
+            $table->binary('PrefecturesIcon');
+
+            $table->foreign('RegionID')->references('RegionID')->on('region');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prefectures');
+    }
+}
