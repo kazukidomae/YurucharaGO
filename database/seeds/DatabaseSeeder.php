fa57a772b67dc4da8de67f9a87869e6a8751f98a
kazukidoumae <donmainakanaideyo@gmail.com>
2018-06-10 22:35:31 +0900

    [add]EventsSeeder and CardsSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RegionsTableSeeder::class,
             PrefecturesTableSeeder::class,
+            CardsTableSeeder::class,
+            EventsTableSeeder::class,
         ]);
     }
 }
