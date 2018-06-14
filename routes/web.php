9acc61e1fc0eb3f6a7a5ebf3816c13e4b3e5d5c0
nakajima.shunsuke <grimmalice10@gmail.com>
2018-06-14 11:37:29 +0900

    [add]view routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,17 @@ Route::get('/','PagesController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 // 現在地検索
 Route::get('/search', 'MapViewController@search');
+
 // カードリスト表示
-Route::get('/data', 'GetDataController@carddata');
+//Route::get('/data', 'GetDataController@carddata');
+
+//仮でルーティング
+Route::view('/cardlist', 'cardlist');   //検索カード一覧
+Route::view('/obtaincardlist', 'obtaincardlist');   //入手カード一覧
+Route::view('/carddetail', 'carddetail');   //カード詳細
+Route::view('/trophylist', 'trophylist');   //トロフィー一覧
+Route::view('/trophydetail', 'trophydetail');   //トロフィー詳細
+Route::view('/configuration', 'configuration');   //設定
