bb4ccc0362aae5f329fc99e78075cd58d2e351ce
mayuTakaji <tmayu228@gmail.com>
2018-06-20 18:57:40 +0900

    [update]carddetail style.css

--- resources/views/cardDetail.blade.php
+++ resources/views/cardDetail.blade.php
@@ -2,13 +2,24 @@
 
 @section('content')
     <div class="card">
-        <a href="{{ action('GetDataController@getNavigationData') }}?CardID={{$data[0]->CardID}}" class="card__link">
-            <img src="{{ asset('images/card_layout.png') }}" class="card__img">
-            <img src="{{$data[0]->	PrefecturesIcon}}" class="card__mark">
-            <img src="{{$data[0]->CardIllustration}}" class="card__chara">
-            <div class="textbox">
-                <div class="textbox__name">{{$data[0]->CardName}}</div>
-            </div>
-        </a>
+      <img src="{{ asset('images/wallpaper.png') }}" class="card__img">
+      <img src="{{$data[0]->	PrefecturesIcon}}" class="card__mark card__mark--detail">
+      <img src="{{$data[0]->CardIllustration}}" class="card__chara">
+      <div class="card__name"><h2>{{$data[0]->CardName}}</h2></div>
+      <div class="info">
+        <div class="info__map">
+          <p>生息地：<a href="{{ action('GetDataController@getNavigationData') }}?CardID={{$data[0]->CardID}}">熊本県</a></p>
+        </div>
+        <div class="info__text">
+          <p>{{ $data[0]-> Introduction }}</p>
+          <p></p>
+        </div>
+        <div class="info__images">
+          <img src="images/img_profile.png">
+          <img src="images/map.png">
+          <img src="images/img_profile.png">
+          <img src="images/map.png">
+        </div>
+      </div>
     </div>
 @endsection
