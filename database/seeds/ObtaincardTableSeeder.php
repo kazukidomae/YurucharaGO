b5c45728f8f536cf1766a33f55112c6067bf1842
kazukidoumae <donmainakanaideyo@gmail.com>
2018-07-25 23:06:53 +0900

    [add]testdata

--- database/seeds/ObtaincardTableSeeder.php
+++ database/seeds/ObtaincardTableSeeder.php
@@ -20,56 +20,126 @@ class ObtaincardTableSeeder extends Seeder
             ],
             [
                 'UserID' => 2,
-                'CardID' => 45,
+                'CardID' => 43,
                 'ObtainDate' => '2018-07-26',
             ],
             [
                 'UserID' => 2,
-                'CardID' => 50,
-                'ObtainDate' => '2018-07-26',
-            ],
-            [
-                'UserID' => 2,
-                'CardID' => 53,
-                'ObtainDate' => '2018-07-26',
-            ],
-            [
-                'UserID' => 2,
-                'CardID' => 23,
-                'ObtainDate' => '2018-07-26',
-            ],
-            [
-                'UserID' => 3,
-                'CardID' => 18,
-                'ObtainDate' => '2018-07-26',
-            ],
-            [
-                'UserID' => 3,
-                'CardID' => 51,
-                'ObtainDate' => '2018-07-26',
-            ],
-            [
-                'UserID' => 3,
-                'CardID' => 4,
-                'ObtainDate' => '2018-07-26',
-            ],
-            [
-                'UserID' => 3,
-                'CardID' => 12,
-                'ObtainDate' => '2018-07-26',
-            ],
-            [
-                'UserID' => 4,
                 'CardID' => 6,
                 'ObtainDate' => '2018-07-26',
             ],
             [
-                'UserID' => 4,
-                'CardID' => 25,
+                'UserID' => 2,
+                'CardID' => 15,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 11,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 38,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 51,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 40,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 22,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 1,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 52,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 3,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 13,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 24,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 35,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 2,
+                'CardID' => 41,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 3,
+                'CardID' => 50,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 3,
+                'CardID' => 53,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 3,
+                'CardID' => 23,
                 'ObtainDate' => '2018-07-26',
             ],
             [
                 'UserID' => 4,
+                'CardID' => 18,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 4,
+                'CardID' => 51,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 4,
+                'CardID' => 4,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 4,
+                'CardID' => 12,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 5,
+                'CardID' => 6,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 5,
+                'CardID' => 25,
+                'ObtainDate' => '2018-07-26',
+            ],
+            [
+                'UserID' => 5,
                 'CardID' => 52,
                 'ObtainDate' => '2018-07-26',
             ],
